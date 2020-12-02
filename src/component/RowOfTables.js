13f09cfc69f5db89d9bc4fc44aceefa9bd290b6c
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-12-02 18:08:34 +0200

    table page implementation

---
+++ src/component/RowOfTables.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './RowOfTables.css';
+
+function RowOfTables({onTableClick}) {
+    const height = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+    return (
+        <div className="row-tables">
+            {
+                height.map(x => {
+                    return <div key={x} onClick={() => onTableClick(x)} className="table"/>
+                })
+            }
+        </div>
+
+    );
+}
+
+export default RowOfTables;
--- src/page/editor/EditorPage.js
+++ src/page/editor/EditorPage.js
@@ -6,10 +6,16 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import {withStyles} from "@material-ui/core/styles";
 import styles from "./EditorPage.style";
+import RowOfTables from "../../component/RowOfTables";
 
 
 function EditorPage(props) {
     const { classes } = props;
+    const vertical = [0,1,2,3,4,5,6,7,8,9];
+
+    const tableClickHandler = (x, y) => {
+        alert(`x : ${x} - y : ${y}`);
+    };
 
     return (
         <Paper className={classes.paper}>
@@ -18,15 +24,18 @@ function EditorPage(props) {
                     <Grid container spacing={2} alignItems="center">
                         <Grid item xs>
                             <Typography color="textPrimary">
+                                Tables
                             </Typography>
                         </Grid>
                     </Grid>
                 </Toolbar>
             </AppBar>
             <div className={classes.contentWrapper}>
-                <Typography color="textSecondary" align="center">
-                    Editor page
-                </Typography>
+                {
+                    vertical.map(y =>
+                        <RowOfTables onTableClick={(x) => tableClickHandler(x, y)} key={y}/>
+                        )
+                }
             </div>
         </Paper>
     );
