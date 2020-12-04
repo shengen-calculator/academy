e32b5f8c348e06520d5b90c3b906e848991fcc0a
Vasyl Suddya <Vasiliy.Suddya@eu.jll.com>
2020-12-04 23:49:33 +0200

    drag and drop fixes

--- src/component/RowOfTables.js
+++ src/component/RowOfTables.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './RowOfTables.css';
 
-function RowOfTables({onTableClick, onTableDragStart, onTableDragOver,onTableDragEnd, data}) {
+function RowOfTables({onTableClick, onTableDragStart, onTableDragOver, data}) {
     const height = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
     return (
@@ -9,11 +9,10 @@ function RowOfTables({onTableClick, onTableDragStart, onTableDragOver,onTableDra
             {
                 height.map(x => {
                     const value = data.find(tab => tab.x === x);
-                    return <div key={x} className="table-shell" onDragOver={() => onTableDragOver(x)} >
+                    return <div key={x} className="table-shell" onDragOver={(e) => onTableDragOver(e, x)} >
                         <div draggable
                              onClick={() => onTableClick(x)}
-                             onDragStart={(e) => onTableDragStart(e, x)}
-                             onDragEnd={onTableDragEnd}
+                             onDragStart={(e) => onTableDragStart(e, x, value.id)}
                              className="table"
                         >
                             {value ? value.seats : ''}
